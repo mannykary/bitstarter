b94e13180dcf6ed6013311aaaaca98a2d6bb50f4
mannykary <mrkaryam@gmail.com>
2013-07-06 20:02:31 -0700

    updated web.js to point to index.html

--- web.js
+++ web.js
@@ -2,9 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString());
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
