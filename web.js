68f4dfd9f3872d180f6fb8e2c1cc8c9e35f93624
mannykary <mrkaryam@gmail.com>
2013-07-06 20:08:27 -0700

    fixing web.js so it points to index.html

--- web.js
+++ web.js
@@ -2,7 +2,9 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = fs.readFileSync('index.html');
+var buffer = new Buffer(16);
+
+buffer = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
   response.send(buffer.toString());
